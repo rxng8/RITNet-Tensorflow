9cea7e57956bcd2755176847b0185e2cb6ebcd34
Alex Nguyen <alexvn.edu@gmail.com>
2022-05-03 12:43:32 -0400

    fix hyperparameter name and implement bal loss function

--- notebooks/bal_loss.py
+++ notebooks/bal_loss.py
@@ -28,7 +28,7 @@ if gpus:
     # Memory growth must be set before GPUs have been initialized
     print(e)
 
-config_path = "../configs/training_config/training_config_2.json"
+config_path = "../configs/training_config/training_config_3.json"
 model_config_path = "../configs/model_config/simplenet.json"
 
 config = get_config_from_json(config_path)
@@ -43,6 +43,7 @@ from ritnet.utils.config import GLOBAL_CONFIG
 # GLOBAL_CONFIG, we have to import them after we set the config
 from ritnet.model.model_builder import build_unet_model
 from ritnet.utils.utils import preprocess_image, preprocess_label, show_img, show_imgs
+from ritnet.dataloader.dataloader import encode_label_to_distance_matrix
 
 # Define path
 image_path = "../data/s-general/7/synthetic/0011.tif"
@@ -67,21 +68,43 @@ except:
 assert label.shape[-1] == 3, "Inconsistent label."
 
 prep_image = preprocess_image(image, image_size=(GLOBAL_CONFIG.image_size.height, GLOBAL_CONFIG.image_size.width))[tf.newaxis, ...]
-prep_label = preprocess_label(label, image_size=(GLOBAL_CONFIG.image_size.height, GLOBAL_CONFIG.image_size.width))[tf.newaxis, ...]
+prep_label = preprocess_label(label, image_size=(GLOBAL_CONFIG.image_size.height, GLOBAL_CONFIG.image_size.width))
+dist_matrix = encode_label_to_distance_matrix(prep_label)
+prep_label = prep_label[tf.newaxis, ...]
+dist_matrix = dist_matrix[tf.newaxis, ...]
 
+# img = tf.cast(prep_label[0, ...], tf.uint8) * 255
+# img = img.numpy()
+# edges_whole = cv.Canny(img[..., 0], 100, 200)
+# edges_pupil = cv.Canny(img[..., 1], 100, 200)
+# edges_iris = cv.Canny(img[..., 2], 100, 200)
+# edges_sclera = cv.Canny(img[..., 3], 100, 200)
+
+# show_img(prep_image[0, ...])
+# show_imgs([[edges_whole, edges_pupil], [edges_iris, edges_sclera]])
 
 # %%
 
-img = tf.cast(prep_label[0, ...], tf.uint8) * 255
-img = img.numpy()
-edges_whole = cv.Canny(img[..., 0], 100, 200)
-edges_pupil = cv.Canny(img[..., 1], 100, 200)
-edges_iris = cv.Canny(img[..., 2], 100, 200)
-edges_sclera = cv.Canny(img[..., 3], 100, 200)
+channel = 1
+delta = 5 # pixel
+omega_0 = 10 # weights
+omega_1 = 1e4 # weigths for class invariance
 
-show_imgs([[edges_whole, edges_pupil], [edges_iris, edges_sclera]])
+mask_dist_matrix = tf.cast(dist_matrix[0, ...], tf.float32) * (1 - prep_label[0, ...])
+
+flat_true = tf.reshape(prep_label, (1, -1, GLOBAL_CONFIG.n_class))[0, ...]
+invariance_per_class = 1 / (tf.pow(tf.reduce_sum(flat_true, axis=-2), 2) + 1e-8)
+
+invariance_per_class *= omega_1
+
+bal_loss = omega_0 * tf.exp( - ( tf.square(mask_dist_matrix) / (2 * (delta ** 2)) ))
+print (invariance_per_class)
+
+invariance_per_class = invariance_per_class[tf.newaxis, tf.newaxis, ...]
+invariance_per_class = tf.broadcast_to(invariance_per_class, shape=bal_loss.shape)
 
 
-# %%
+bal_loss *= invariance_per_class
 
-show_img(prep_image[0, ...])
+bal_loss = tf.reduce_mean(bal_loss)
+print(bal_loss)
--- ritnet/trainer/loss.py
+++ ritnet/trainer/loss.py
@@ -42,17 +42,45 @@ def get_loss_func_by_loss_config(loss_config: Munch):
 def get_gdl_bal_sl_loss_func(loss_config: Munch, verbose=True):
   if verbose:
     print("[Loss] get_gdl_bal_sl_loss")
-  pass
+
+  def bal_gdl_sl_loss(true, pred, dist_matrix):
+    cel = ce_loss(true, pred)
+    bal_weights = bal_weight_map(loss_config, true, dist_matrix)
+    bal = cel * bal_weights
+    gdl = gdl_loss(loss_config, pred, dist_matrix)
+    sl = sl_loss(loss_config, pred, dist_matrix)
+
+    return bal + gdl + sl
+
+  return bal_gdl_sl_loss
 
 def get_gdl_bal_loss_func(loss_config: Munch, verbose=True):
   if verbose:
     print("[Loss] get_gdl_bal_loss")
-  pass 
+
+  def bal_gdl_loss(true, pred, dist_matrix):
+    cel = ce_loss(true, pred)
+    bal_weights = bal_weight_map(loss_config, true, dist_matrix)
+    bal = cel * bal_weights
+    gdl = gdl_loss(loss_config, pred, dist_matrix)
+
+    return bal + gdl
+  
+  return bal_gdl_loss
 
 def get_bal_sl_loss_func(loss_config: Munch, verbose=True):
   if verbose:
     print("[Loss] get_bal_sl_loss")
-  pass 
+
+  def bal_sl_loss(true, pred, dist_matrix):
+    cel = ce_loss(true, pred)
+    bal_weights = bal_weight_map(loss_config, true, dist_matrix)
+    bal = cel * bal_weights
+    surface_loss = sl_loss(loss_config, pred, dist_matrix)
+
+    return bal + surface_loss
+
+  return bal_sl_loss
 
 def get_gdl_sl_loss_func(loss_config: Munch, verbose=True):
   if verbose:
@@ -81,7 +109,13 @@ def get_gdl_loss_func(loss_config: Munch, verbose=True):
 def get_bal_loss_func(loss_config: Munch, verbose=True):
   if verbose:
     print("[Loss] get_bal_loss")
-  pass 
+
+  def bal_loss(true, pred, dist_matrix):
+    cel = ce_loss(true, pred)
+    bal_weights = bal_weight_map(loss_config, true, dist_matrix)
+    return cel * bal_weights
+
+  return bal_loss
 
 def get_sl_loss_func(loss_config: Munch, verbose=True):
   if verbose:
@@ -124,13 +158,22 @@ def gdl_loss(loss_config, true, pred) -> tf.Tensor:
 
   gdl = tf.reduce_sum(gdl_b)
 
-  return loss_config.gdl_theta * gdl
+  return loss_config.gdl_lambda * gdl
 
 def sl_loss(loss_config, pred, dist_matrix) -> tf.Tensor:
   assert pred.shape == dist_matrix.shape
   sigmoid_pred = tf.nn.sigmoid(pred)
   surface_loss = tf.reduce_mean(sigmoid_pred * dist_matrix)
-  return loss_config.sl_theta * surface_loss
+  return loss_config.sl_lambda * surface_loss
 
-def bal_loss(loss_config, true, pred):
-  pass
+def bal_weight_map(loss_config, true, dist_matrix):
+  mask_dist_matrix = tf.cast(dist_matrix, tf.float32) * (1 - true)
+  flat_true = tf.reshape(true, (GLOBAL_CONFIG.batch_size, -1, GLOBAL_CONFIG.n_class))
+  invariance_per_class = 1 / (tf.pow(tf.reduce_sum(flat_true, axis=-2), 2) + 1e-8)
+  invariance_per_class *= loss_config.bal_omega_1
+  bal_weights = loss_config.bal_omega_0 * tf.exp( - ( tf.square(mask_dist_matrix) / (2 * (loss_config.bal_delta ** 2)) ))
+  invariance_per_class = invariance_per_class[tf.newaxis, tf.newaxis, ...]
+  invariance_per_class = tf.broadcast_to(invariance_per_class, shape=bal_weights.shape)
+  bal_weights *= invariance_per_class
+  bal_weights = tf.reduce_mean(bal_weights)
+  return loss_config.bal_lambda_1 + loss_config.bal_lambda_2 * bal_weights
